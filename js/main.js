1a8856df03d30009a578efb934c9696f4e8886d9
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-06-08 13:03:55 -0700

    Add a function that uses the http GET method to request movie data. #1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-var $spinWheel = document.querySelector('spin-wheel-button');
+var $spinWheel = document.querySelector('.spin-wheel-button');
 
-$spinWheel.addEventListener('click', function () {});
+$spinWheel.addEventListener('click', getMovie);
+
+function getMovie(event) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.themoviedb.org/3/discover/movie?api_key=a5e47a4e0a5f7197c6934d0fb4135ec4&language=en-US&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+  });
+  xhr.send();
+}
